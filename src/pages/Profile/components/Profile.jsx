ca426e9c14883801297fd193f923ce36c64312fe
taydrey <taydrey@users.noreply.github.com>
2020-05-25 12:34:29 +0100

    create profile page boilerplate

---
+++ src/pages/Profile/components/Profile.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../../components/Header';
+import {
+    LayoutWrapper, WidthWrapperCenter
+} from '../../components/reusablestyles/GlobalStyle';
+
+import SideNav from '../../components/SideNav';
+
+const Profile = () => (
+    <LayoutWrapper>
+        <WidthWrapperCenter>
+            <SideNav />
+            <Header />
+        </WidthWrapperCenter>
+    </LayoutWrapper>
+);
+
+export default Profile;
